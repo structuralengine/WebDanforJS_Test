e790fac9c0fe4db4ac65fb5684a653e4a84566db
yuluji <yuji.hirano@malme.co.jp>
2023-01-23 15:54:29 +0900

    Enterでフォーカスをせずに右のセルへ行くように修正

--- src/app/components/sheet/sheet.component.ts
+++ src/app/components/sheet/sheet.component.ts
@@ -13,37 +13,73 @@ import 'pqgrid/localize/pq-localize-ja.js';
 export class SheetComponent implements AfterViewInit, OnChanges {
 
   @ViewChild('pqgrid') div: ElementRef;
-  @Input() options:any;
+  @Input() options: any;
   grid: pq.gridT.instance = null;
 
-  private createGrid(){
-      this.grid = pq.grid(this.div.nativeElement, this.options);
-  }
-
-  ngOnChanges(obj: SimpleChanges){
-      if( !obj.options.firstChange ){
-        //grid is destroyed and recreated only when whole options object is changed to new reference.
-        this.grid.destroy();
-        this.createGrid();
+  private createGrid() {
+    this.options.beforeCellKeyDown = (evt, ui) => {
+      // Shiftを押したら左へ移動
+      let mov = 1;
+      if (evt.shiftKey === true) {
+        mov = -1;
       }
+      if (evt.key === 'Enter') {
+        const $cell = this.grid.getCell({
+          rowIndx: ui.rowIndx,
+          colIndx: ui.colIndx + mov,
+        });
+
+        if ($cell.length > 0) {
+          // 右に移動
+          this.grid.setSelection({
+            rowIndx: ui.rowIndx,
+            colIndx: ui.colIndx + mov,
+            focus: true,
+          });
+        } else {
+          // 次の行の左端に移動
+          this.grid.setSelection({
+            rowIndx: ui.rowIndx + mov,
+            colIndx: 0,
+            focus: true,
+          });
+
+          this.grid.scrollX(0);
+        }
+
+        return false;
+      }
+
+      return true;
+    };
+
+    this.grid = pq.grid(this.div.nativeElement, this.options);
   }
 
-  ngAfterViewInit(){ 
+  ngOnChanges(obj: SimpleChanges) {
+    if (!obj.options.firstChange) {
+      //grid is destroyed and recreated only when whole options object is changed to new reference.
+      this.grid.destroy();
       this.createGrid();
+    }
   }
 
-  refreshDataAndView(){
-    if (this.grid === null ) {
+  ngAfterViewInit() {
+    this.createGrid();
+  }
+
+  refreshDataAndView() {
+    if (this.grid === null) {
       return;
     }
     this.grid.refreshDataAndView();
   }
 
-  refresh(){
-    if (this.grid === null ) {
+  refresh() {
+    if (this.grid === null) {
       return;
     }
     this.grid.refresh();
   }
-  
+
 }
