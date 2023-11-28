ad4a8dd6c93217d8f7350063b37d14b6601a8496
tuananhduong1811 <152245763+tuananhduong1811@users.noreply.github.com>
2023-11-28 14:50:26 +0700

    change file scss

--- src/app/components/material-strength-verification-conditions/material-strength-verification-conditions.component.ts
+++ src/app/components/material-strength-verification-conditions/material-strength-verification-conditions.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-material-strength',
   templateUrl: './material-strength-verification-conditions.component.html',
-  styleUrls: ['./material-strength-verification-conditions.component.css']
+  styleUrls: ['./material-strength-verification-conditions.component.scss'],
 })
 export class MaterialStrengthVerificationConditionComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    console.log("run");
   }
 
 }
